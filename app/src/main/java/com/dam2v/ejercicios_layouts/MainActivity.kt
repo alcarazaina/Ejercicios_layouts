8bb35d3e2a20ad2423b43308abdf1225d2f1b626
alcarazaina <alcarazdainara@gmail.com>
2024-10-09 22:38:26 +0200

    update

--- app/src/main/java/com/dam2v/ejercicios_layouts/MainActivity.kt
+++ app/src/main/java/com/dam2v/ejercicios_layouts/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,7 +14,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dam2v.ejercicios_layouts.ui.theme.Ejercicios_layoutsTheme
@@ -41,9 +44,23 @@ fun Box(modifier: Modifier = Modifier) {
         modifier = modifier.padding(vertical = 20.dp, horizontal = 20.dp)
     ) {
         Row(modifier = Modifier.padding(20.dp)) {
+            Surface (
+                modifier = modifier.padding(vertical = 20.dp, horizontal = 20.dp),
+                color = MaterialTheme.colorScheme.tertiary
+            ){
+                Column (
+                    modifier = modifier.weight(20f).padding(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally){
+
+                }
+            }
+        }
+        Row(modifier = Modifier.padding(20.dp)) {
             Column(
-                modifier = modifier.weight(20f)
-                    .padding()
+                modifier = modifier.weight(20f).padding(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text(text = "Esto es un ejemplo")
             }
--- app/src/main/java/com/dam2v/ejercicios_layouts/ui/theme/Color.kt
+++ app/src/main/java/com/dam2v/ejercicios_layouts/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.dam2v.ejercicios_layouts.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
+val Purple80 = Color(0xFF1D5396)
 val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Pink80 = Color(0xFFDC0C48)
+val LightBlue = Color(0xFF85C2E5)
 
-val Purple40 = Color(0xFF6650a4)
+val Purple40 = Color(0xFF0919A8)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
